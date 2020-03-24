169e59a00160b69c69218a6ec2d9d6002ce588cf
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2020-03-24 10:38:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-62310045"; ?><br>
+<?php echo "ชื่อ-สกุล นาย KHORN SOKHENG"; ?><br>
+<iframe width="500" height="500"
+src="https://youtu.be/fRhiNdpSNcc">
+</iframe>
+</BODY>
+</HTML>
