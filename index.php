ace7fe43822b71d040f25a3b3281165e812ddbc3
KhornSokheng <59908402+KhornSokheng@users.noreply.github.com>
2020-03-24 11:02:04 +0700

    Update index.php
    Third Edition: Update Youtube link

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <?php echo "รหัสนิสิต-62310045"; ?><br>
 <?php echo "ชื่อ-สกุล นาย KHORN SOKHENG"; ?><br>
 <iframe width="500" height="500"
-src="https://youtube/fRhiNdpSNcc">
+src="https://www.youtube.com/watch?v=fRhiNdpSNcc">
 </iframe>
 </BODY>
 </HTML>
